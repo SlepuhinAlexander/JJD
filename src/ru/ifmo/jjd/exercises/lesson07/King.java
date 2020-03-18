d07d8933f5b22cbc83574076bac790b19e570174
Alexander Slepuhin <slepuhinalexander@yandex.ru>
2020-03-18 21:44:59 +0300

    Added Lesson 07 draft

---
+++ src/ru/ifmo/jjd/exercises/lesson07/King.java
@@ -0,0 +1,22 @@
+package ru.ifmo.jjd.exercises.lesson07;
+
+public class King extends Unit {
+    private int gold;
+
+    public King(int healthScore, int speed) {
+        super(healthScore, speed);
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    @Override
+    public void rest() {
+        System.out.println("King rests");
+    }
+}
---
+++ src/ru/ifmo/jjd/exercises/lesson07/Unit.java
@@ -0,0 +1,52 @@
+package ru.ifmo.jjd.exercises.lesson07;
+
+abstract public class Unit implements CanRest{
+    protected String name = "Unit";
+    /*
+     * Модификатор доступа protected указывает, что данное свойство доступно всем потомкам данного класса, и всем
+     * классам в этом же пакете
+     * */
+    protected int healthScore;
+    protected int speed;
+
+    public Unit(int healthScore, int speed) {
+        setHealthScore(healthScore);
+        setSpeed(speed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealthScore() {
+        return healthScore;
+    }
+
+    public void setHealthScore(int healthScore) {
+        this.healthScore = healthScore;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    /*
+     * Дополнительные методы, не гетеры и сеттеры, принято описывать после геттеров и сеттеров.
+     * */
+
+    public boolean isAlive() {
+        return healthScore > 0;
+    }
+
+    public void run() {
+        System.out.println(name + " двигается со скоростью " + speed);
+    }
+}
