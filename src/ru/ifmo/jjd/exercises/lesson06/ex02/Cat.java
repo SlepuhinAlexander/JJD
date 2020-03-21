03c290b69495d22dea68346e4b8d446d3e63bd3a
Alexander <slepuhinalexander@yandex.ru>
2020-03-21 21:49:25 +0300

    Added lost class Cat for ex02 in lesson 06

--- src/ru/ifmo/jjd/exercises/lesson06/ex02/Cat.java
+++ src/ru/ifmo/jjd/exercises/lesson06/ex02/Cat.java
@@ -1,21 +1,44 @@
 package ru.ifmo.jjd.exercises.lesson06.ex02;
 
 public class Cat {
-    private String name;
-    private double weight;
-    private int age;
-    private String colour;
-    private String ownerAddress;
-    private double health;
-    private double hitPower;
+    private String name = "Кот";
+    private int age = 1;
+    private int weight = 4;
+    private String colour = "Чёрный";
+    private String ownerAddress = "";
+    private int healthScore = 100;
+    private int hitPower = 1;
 
-    public Cat(String name, String colour) {
+    public Cat(String name) {
         setName(name);
     }
 
-    public Cat(double health, String ownerAddress, double hitPower) {
-        setHealth(health);
+    public Cat(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public Cat(String name, int age, int weight) {
+        setName(name);
+        setAge(age);
+        setWeight(weight);
+    }
+
+    public Cat(String name, String colour) {
+        setName(name);
+        setColour(colour);
+    }
+
+    public Cat(String name, int age, String ownerAddress) {
+        setName(name);
+        setAge(age);
         setOwnerAddress(ownerAddress);
+    }
+
+    public Cat(String name, String colour, int healthScore, int hitPower) {
+        setName(name);
+        setColour(colour);
+        setHealthScore(healthScore);
         setHitPower(hitPower);
     }
 
@@ -29,12 +52,12 @@ public class Cat {
         }
     }
 
-    public double getWeight() {
+    public int getWeight() {
         return weight;
     }
 
-    public void setWeight(double weight) {
-        if (weight > 0.0) {
+    public void setWeight(int weight) {
+        if (weight > 0) {
             this.weight = weight;
         }
     }
@@ -69,22 +92,20 @@ public class Cat {
         }
     }
 
-    public double getHealth() {
-        return health;
+    public int getHealthScore() {
+        return healthScore;
     }
 
-    public void setHealth(double health) {
-        if (health > 0.0) {
-            this.health = health;
-        }
+    public void setHealthScore(int healthScore) {
+        this.healthScore = Math.max(healthScore, 0);
     }
 
-    public double getHitPower() {
+    public int getHitPower() {
         return hitPower;
     }
 
-    public void setHitPower(double hitPower) {
-        if (hitPower > 0.0) {
+    public void setHitPower(int hitPower) {
+        if (hitPower > 0) {
             this.hitPower = hitPower;
         }
     }
@@ -97,13 +118,25 @@ public class Cat {
                 ", возраст = " + age +
                 ", цвет = '" + colour + '\'' +
                 ", адрес владельца = '" + ownerAddress + '\'' +
-                ", здововье = " + health +
+                ", здововье = " + healthScore +
                 ", сила удара = " + hitPower +
                 '}';
     }
 
     public void fight(Cat anotherCat) {
-        setHealth(getHealth() - anotherCat.getHitPower());
-        anotherCat.setHealth(anotherCat.getHealth() - getHitPower());
+        System.out.print(name + " наносит удар " + hitPower + " по " + anotherCat.name + " : ");
+        anotherCat.setHealthScore(anotherCat.healthScore - hitPower);
+        System.out.println("осталось здоровья " + anotherCat.healthScore);
+        if (anotherCat.healthScore == 0) {
+            System.out.println(anotherCat.name + " не может больше драться.");
+            return;
+        }
+
+        System.out.print(anotherCat.name + " наносит ответный удар " + anotherCat.hitPower + " по " + name + " : ");
+        setHealthScore(healthScore - anotherCat.hitPower);
+        System.out.println("осталось здоровья " + healthScore);
+        if (healthScore == 0) {
+            System.out.println(name + " не может больше драться.");
+        }
     }
 }
